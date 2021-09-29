120ec8b08406df12cd1e84910c88d708006a739b
d-uni <84150950+d-uni@users.noreply.github.com>
2021-09-29 21:06:03 +0300

    Update 04-3.c

--- 04-3.c
+++ 04-3.c
@@ -14,18 +14,19 @@ enum
     (int argc, char **argv)
     {
     long long int max1 = LLONG_MIN, max2 = LLONG_MIN;
-    int sum = 0; ///unsigned int
+    int sum = 0;                     ///unsigned int sum = 0;  и   int sumi = 0;
     int fd, n;
     unsigned char buf[20] = {};
     for(int i = 1; i < argc; i++) {
     fd = open(argv[i],  O_RDONLY);
     while((n = read(fd, buf, sizeof(buf))) != 0) {
         sum = buf[16] << 24 | buf[17] << 16 | buf[18] << 8 | buf[19];
-        if(sum > max2) {
-            max1 = max2;
-            max2 = sum;
-        } else if((sum > max1) && (sum != max2)) {
-            max1 = sum;
+                                                           /// sumi = (int)sum;
+        if(sum > max2) {                                   ///if(sumi > max2) {
+            max1 = max2;                                   ///max1 = max2;
+            max2 = sum;                                    /// max2 = sumi;
+        } else if((sum > max1) && (sum != max2)) {         /// } else if((sumi > max1) && (sumi != max2)) {
+            max1 = sum;                                    ///  max1 = sumi;}
             }
         }
     close(fd);
@@ -33,7 +34,7 @@ enum
     if(max1 !=  LLONG_MIN ) {
         int res = (int)max1;
         if(res < 0) {
-        res = -res;
+        res = -res; /// res = ~res+1
         printf("-%dg%02ds%02dk\n",res/CONVERT_GAL, res%CONVERT_GAL/CONVERT_SIK, res % CONVERT_SIK);
     } else {
         printf("%dg%02ds%02dk\n",res/ CONVERT_GAL, res% CONVERT_GAL / CONVERT_SIK, res % CONVERT_SIK);
