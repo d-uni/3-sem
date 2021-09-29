8b9c9e597408cd9742c9f2d77187cb639af05ae3
d-uni <84150950+d-uni@users.noreply.github.com>
2021-09-29 22:39:26 +0300

    Update 04-3.c

--- 04-3.c
+++ 04-3.c
@@ -14,30 +14,29 @@ enum
     (int argc, char **argv)
     {
     long long int max1 = LLONG_MIN, max2 = LLONG_MIN;
-    int sum = 0;                     ///unsigned int sum = 0;  и   int sumi = 0;
-    int fd, n;
+    int sum = 0;
+    int fd;
     unsigned char buf[20] = {};
     for(int i = 1; i < argc; i++) {
     fd = open(argv[i],  O_RDONLY);
-    while((n = read(fd, buf, sizeof(buf))) != 0) {
-        sum = buf[16] << 24 | buf[17] << 16 | buf[18] << 8 | buf[19];
-                                                           /// sumi = (int)sum;
-        if(sum > max2) {                                   ///if(sumi > max2) {
-            max1 = max2;                                   ///max1 = max2;
-            max2 = sum;                                    /// max2 = sumi;
-        } else if((sum > max1) && (sum != max2)) {         /// } else if((sumi > max1) && (sumi != max2)) {
-            max1 = sum;                                    ///  max1 = sumi;}
+    while(read(fd, buf, sizeof(buf)) != 0) {
+        sum = (unsigned)buf[16] << 24 | (unsigned)buf[17] << 16 | (unsigned)buf[18] << 8 | buf[19];
+        if(sum > max2) {
+            max1 = max2;
+            max2 = sum;
+        } else if((sum > max1) && (sum != max2)) {
+            max1 = sum;
             }
         }
     close(fd);
     }
     if(max1 !=  LLONG_MIN ) {
-        int res = (int)max1;
+        long long int res = max1;
         if(res < 0) {
-        res = -res; /// res = ~res+1
-        printf("-%dg%02ds%02dk\n",res/CONVERT_GAL, res%CONVERT_GAL/CONVERT_SIK, res % CONVERT_SIK);
+        res = -res;
+        printf("-%lldg%02llds%02lldk\n",res/CONVERT_GAL, res%CONVERT_GAL/CONVERT_SIK, res%CONVERT_SIK);
     } else {
-        printf("%dg%02ds%02dk\n",res/ CONVERT_GAL, res% CONVERT_GAL / CONVERT_SIK, res % CONVERT_SIK);
+        printf("%lldg%02llds%02lldk\n",res/ CONVERT_GAL, res%CONVERT_GAL/CONVERT_SIK, res%CONVERT_SIK);
         }
     }
     return 0;
