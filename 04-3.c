7555d972b2551a21a8a821a0e50e049a5fb5fd5b
d-uni <84150950+d-uni@users.noreply.github.com>
2021-09-29 20:52:25 +0300

    Update 04-3.c

--- 04-3.c
+++ 04-3.c
@@ -14,13 +14,12 @@ enum
     (int argc, char **argv)
     {
     long long int max1 = LLONG_MIN, max2 = LLONG_MIN;
-    long long int sum = 0;
+    int sum = 0; ///unsigned int
     int fd, n;
     unsigned char buf[20] = {};
     for(int i = 1; i < argc; i++) {
     fd = open(argv[i],  O_RDONLY);
     while((n = read(fd, buf, sizeof(buf))) != 0) {
-        sum = 0;
         sum = buf[16] << 24 | buf[17] << 16 | buf[18] << 8 | buf[19];
         if(sum > max2) {
             max1 = max2;
