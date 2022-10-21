4f9d7e6ec7624a37ed8aec9cd60281fb4a995ab9
d-uni <84150950+d-uni@users.noreply.github.com>
2022-10-21 19:19:30 +0300

    Add files via upload

---
+++ smain2.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+ int main()
+ {int x = 0; int reject = 1; int summon = 2; int disqualify = 3; int answ = -1;
+while((scanf("%d", &x)) > 0) {
+ answ = w e r ;
+ if(answ == 1) printf("reject\n");
+if(answ == 2) printf("summon\n");
+if(answ == 3) printf("disqualify\n");
+}return 0;
+}
